b1c946550683fa91c4f5f29d05a0bd4c78ab7fc0
ArihaIngale <arihaingale220705@gmail.com>
2025-05-07 14:58:15 +0530

    employee

--- employee.java
+++ employee.java
@@ -1,22 +1,24 @@
 public class employee{
     String name;
     int id;
+    int age;
     double salary;
-    employee(String name, int id,double salary)
+    employee(String name, int id, int age,double salary)
     {
         this.name=name;
         this.id=id;
-
+        this.age=age;
         this.salary=salary;
     }
     void displayInfo(){
         System.out.println("Employee Id: "+id);
         System.out.println("Employee Name: "+name);
+        System.out.println("Employee Age: "+age);
         System.out.println("Employee Salary: "+salary);
     }
     public static void main(String[] args)
     {
-        employee emp1= new employee("Shreya", 100, 50000.0);
+        employee emp1= new employee("Shreya", 100, 20, 500000.0);
         emp1.displayInfo();
     }
 }
